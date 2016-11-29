c068683f7369efb94dfc17d92bd8a41bc13e15da
Heitor Neto <heitorh3@gmail.com>
2016-11-29 15:23:37 -0200

    Implementando o equals e o hashCode.

--- src/main/java/br/com/model/modelo/Fabricante.java
+++ src/main/java/br/com/model/modelo/Fabricante.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 
 @Entity
 public class Fabricante implements Serializable{
@@ -16,7 +17,9 @@ public class Fabricante implements Serializable{
 	private String nome;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@TableGenerator(name="fabricante_generator", table="GERADOR_CODIGO", pkColumnName="ENTIDADE",
+	valueColumnName="ALOCACAO",allocationSize = 5)
+	@GeneratedValue(generator = "fabricante_generator", strategy = GenerationType.TABLE)
 	public Long getCodigo() {
 		return codigo;
 	}
@@ -31,5 +34,28 @@ public class Fabricante implements Serializable{
 		this.nome = nome;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fabricante other = (Fabricante) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 	
 }
